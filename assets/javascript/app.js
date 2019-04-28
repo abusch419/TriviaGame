9eda1750f4308e00b827d2d996018a29d8364e4b
Abusch419 <abusch419@gmail.com>
2019-04-28 16:02:37 -0400

    fixes links

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -98,9 +98,9 @@ $(document).ready(function () {
   var unanswered = 0;
   var correctAnswerCount = 0;
   var incorrectAnswerCount = 0;
-  var audio = new Audio("/Users/alexandereversbusch1/bootcamp/TriviaGame/assets/images/day25_uenw4a2_mybrotheresau.mp3");
+  var audio = new Audio("https://abusch419.github.io/TriviaGame/assets/images/day25_uenw4a2_mybrotheresau.mp3");
 
-
+ 
 
 
 
